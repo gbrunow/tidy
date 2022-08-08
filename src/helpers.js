0c5c4d14ddd2abbddc7113924b651461ff0d8b09
Gui Brunow Gomes <gbrunow@apple.com>
2022-08-08 13:18:41 -0700

    working plugin

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-export function Node(value, children) {
+export function Node(value, name, children, parent) {
   this.value = value;
   this.children = children || new Set();
 }
@@ -7,11 +7,13 @@ export const getAdjcencyList = (artboards) => {
   const adj = new Map();
 
   for (const artboard of artboards) {
-    let node = adj.get(artboard.name) || new Node([]);
-    node.value.push(artboard);
-    adj.set(artboard.name, node);
+    const name = getName(artboard);
 
-    const parts = artboard.name.split("/");
+    let node = adj.get() || new Node([]);
+    node.value.push(artboard);
+    adj.set(name, node);
+
+    const parts = name.split("/");
     parts.pop();
 
     while (parts.length) {
@@ -27,12 +29,38 @@ export const getAdjcencyList = (artboards) => {
   return adj;
 }
 
+/**
+ * Returns artboard name without comments
+ * (e.g `A/B/C # a comment` becomes `A/B/C`)
+ */
+export const getName = (artboard) => artboard.name.match(/^[^#]*/)[0].trim();
+
 export const getRoots = (artboards, adj) => {
   const roots = new Set();
 
   for (const artboard of artboards) {
-    roots.add(artboard.name.split("/")[0]);
+    roots.add(getName(artboard).split("/")[0]);
   }
 
   return Array.from(roots).map(r => adj.get(r));
 }
+
+export const getGutter = (artboards) => {
+  const MIN_GUTTER = 20;
+  const MAX_GUTTER = 250;
+
+  let totalDimensions = 0;
+  for (const artboard of artboards) {
+    totalDimensions += artboard.frame.height;
+    totalDimensions += artboard.frame.width;
+  }
+
+  const averageDimension = (totalDimensions) / (artboards.length * 2);
+  const gutter = averageDimension / 10;
+
+  return Math.max(Math.min(Math.round(gutter / 10) * 10, MAX_GUTTER), MIN_GUTTER);
+}
+
+export const sortByName = (a, b) => (a.name === b.name ? 0 : a.name < b.name ? -1 : 1);
+
+export const layerIsArtboard = (l) => l.background;
